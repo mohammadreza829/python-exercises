69adf5df902bcb7b08c042b0f676f0ac706330c1
mohammadreza <mohammadrezafasli1234@gmail.com>
2025-10-17 19:52:03 +0330

    matrix

--- python class special exercise/exe1.py
+++ python class special exercise/exe1.py
@@ -1,3 +1,14 @@
-x = int(input("Enter  x number : "))
-p = ((3 * x * x) + x)// 2 
-print(p)
+from random import random , randint
+a = []
+b = []
+for i in range(10):
+    for j in range(10):
+        a.append(randint(0,1))
+    b.append(a)
+    a = []
+for i in range(10):
+    for j in range(10):
+        print(b[i][j], end=' ')
+    print()
+
+
