fd0819980bfec198731d34dfdcb7572a8d7ba654
mohammadreza <mohammadrezafasli1234@gmail.com>
2025-10-19 19:30:08 +0330

    marpich

--- python class special exercise/mrpich adad.py
+++ python class special exercise/mrpich adad.py
@@ -1,5 +1,3 @@
-
-
 x = [
     [25, 24, 23, 22, 21],
     [10, 9, 8, 7, 20],
@@ -8,7 +6,5 @@ x = [
     [13, 14, 15, 16, 17],
 ]
 
-
 a = list(map(lambda row: print(" ".join(f"{num:2}" for num in row)), x))
 print(a)
-
