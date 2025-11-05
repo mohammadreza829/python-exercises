ac6fdb55fde4e5611f4cc2e5151aa92bd732b18d
mohammadreza <mohammadrezafasli1234@gmail.com>
2025-11-05 12:07:48 +0330

    q11

--- data structure exercises/data structure/week6/w6-q1.py
+++ data structure exercises/data structure/week6/w6-q1.py
@@ -1,53 +1,45 @@
-def parse_input_list(s):
-
-    s = s.strip()
-    if s[0] == "[" and s[-1] == "]":
-        s = s[1:-1]
-
-    tokens = s.split(",")
-
-    out = []
-    for tok in tokens:
-        tok = tok.strip()
-
-        if (tok.startswith("'") and tok.endswith("'")) or (
-            tok.startswith('"') and tok.endswith('"')
-        ):
-            tok = tok[1:-1]
-        if tok == "+":
-            out.append("+")
-        elif tok.isdigit():
-            out.append(int(tok))
-        else:
-            print("Error: Invalid Input in list!")
-            exit()
-    return out
-
-
-def simple_calc(num_lst):
-    if not num_lst:
+def stack_based_addition(input_list):
+    if not input_list:
         return "Error: Invalid Input"
-    str_lst = [str(item) for item in num_lst]
-    operator_index = None
-    for i, item in enumerate(str_lst):
+
+    plus_index = -1
+    for idx, item in enumerate(input_list):
         if item == "+":
-            operator_index = i
+            plus_index = idx
             break
-    if operator_index is None:
+
+    if plus_index == -1:
         return "Error: Invalid Input"
-    if operator_index == 0 or operator_index == len(str_lst) - 1:
+    if plus_index == 0 or plus_index == len(input_list) - 1:
         return "Error: Invalid Sequence"
-    left_numbers = "".join(str_lst[:operator_index])
-    right_numbers = "".join(str_lst[operator_index + 1 :])
-    if not left_numbers.isdigit() or not right_numbers.isdigit():
-        return "Error: Calculation Error"
-    left_value = int(left_numbers)
-    right_value = int(right_numbers)
-    result = left_value + right_value
-    return [int(x) for x in str(result)]
+
+    left_digits = "".join(str(x) for x in input_list[:plus_index])
+    right_digits = "".join(str(x) for x in input_list[plus_index + 1 :])
+
+    if not left_digits.isdigit() or not right_digits.isdigit():
+        return "Error: Invalid Input"
+
+    sum_result = int(left_digits) + int(right_digits)
+
+    return [int(d) for d in str(sum_result)]
 
 
-inp = input()
-user_list = parse_input_list(inp)
-result = simple_calc(user_list)
+user_input = input()
+tokens = []
+str_n = ""
+numm = False
+for c in user_input:
+    if c.isdigit():
+        str_n += c
+        numm = True
+    else:
+        if numm:
+            tokens.append(int(str_n))
+            str_n = ""
+            numm = False
+        if c == "+":
+            tokens.append("+")
+if str_n:
+    tokens.append(int(str_n))
+result = stack_based_addition(tokens)
 print(result)
