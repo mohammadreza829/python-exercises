084dc0d60c59735e46275138c75c16adb682fd04
mohammadreza <mohammadrezafasli1234@gmail.com>
2025-11-06 14:46:00 +0330

    ac

--- python review/ramznegari.py
+++ python review/ramznegari.py
@@ -1,16 +1,18 @@
 def ramz(x):
     for i in x:
         z = ord(i)
-        y = z*2-3
+        y = z * 2 - 3
         print(chr(y), end="")
+
+
 def ramgoshaie(x):
     for i in x:
         z = ord(i)
-        y = (z+3)//2
+        y = (z + 3) // 2
         print(chr(y), end="")
+
 
 x = input("enter your chr: ")
 print(ramz(x))
 
 print(ramgoshaie(x))
-        
