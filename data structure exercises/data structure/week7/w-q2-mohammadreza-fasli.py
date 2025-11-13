683ab3d1ae0bfe5d993d4ae19a356e89221e0722
mohammadreza <mohammadrezafasli1234@gmail.com>
2025-11-13 18:54:21 +0330

    input

--- data structure exercises/data structure/week7/w-q2-mohammadreza-fasli.py
+++ data structure exercises/data structure/week7/w-q2-mohammadreza-fasli.py
@@ -1,96 +1,73 @@
-class TrieNode:
+class WordChainSolver:
     def __init__(self):
-        self.children = {}
-        self.is_end_of_word = False
+        self.trie_root = self.TrieNode()
+        self.longest_chain = []
 
+    class TrieNode:
+        def __init__(self):
+            self.children = {}
+            self.is_end_of_word = False
 
-def insert_word(root, word):
-    node = root
-    for char in word:
-        if char not in node.children:
-            node.children[char] = TrieNode()
-        node = node.children[char]
-    node.is_end_of_word = True
+    def insert_word(self, word):
+        node = self.trie_root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = self.TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
 
+    def _dfs_get_words(self, node, prefix, words_list):
+        if node.is_end_of_word:
+            words_list.append(prefix)
+        for char in sorted(node.children.keys()):
+            self._dfs_get_words(node.children[char], prefix + char, words_list)
 
-def _dfs_get_words(node, prefix, words_list):
-    if node.is_end_of_word:
-        words_list.append(prefix)
+    def get_words_starting_with(self, letter):
+        words = []
+        if letter in self.trie_root.children:
+            self._dfs_get_words(self.trie_root.children[letter], letter, words)
+        return words
 
-    for char in sorted(node.children.keys()):
-        _dfs_get_words(node.children[char], prefix + char, words_list)
+    def _backtrack(self, current_chain, used_words):
+        if len(current_chain) > len(self.longest_chain):
+            self.longest_chain = list(current_chain)
+        elif len(current_chain) == len(self.longest_chain):
+            if " ".join(current_chain) < " ".join(self.longest_chain):
+                self.longest_chain = list(current_chain)
 
+        last_word = current_chain[-1]
+        last_char = last_word[-1]
+        candidate_words = self.get_words_starting_with(last_char)
 
-def get_words_starting_with(root, letter):
-    words = []
-    if letter in root.children:
+        for next_word in candidate_words:
+            if next_word not in used_words:
+                used_words.add(next_word)
+                current_chain.append(next_word)
+                self._backtrack(current_chain, used_words)
+                current_chain.pop()
+                used_words.remove(next_word)
 
-        _dfs_get_words(root.children[letter], letter, words)
-    return words
+    def find_longest_chain(self, start_word, all_words):
+        for word in all_words:
+            self.insert_word(word)
 
+        if start_word not in all_words:
+            return [start_word]
 
-longest_chain_found = []
+        self.longest_chain = [start_word]
+        used_words = {start_word}
 
-
-def backtrack(current_chain, used_words, trie_root):
-    global longest_chain_found
-
-    if len(current_chain) > len(longest_chain_found):
-        longest_chain_found = current_chain.copy()
-
-    elif len(current_chain) == len(longest_chain_found):
-        if " ".join(current_chain) < " ".join(longest_chain_found):
-            longest_chain_found = current_chain.copy()
-
-    last_word = current_chain[-1]
-    last_char = last_word[-1]
-
-    candidate_words = get_words_starting_with(trie_root, last_char)
-
-    for next_word in candidate_words:
-        if next_word not in used_words:
-            used_words.add(next_word)
-            current_chain.append(next_word)
-
-            backtrack(current_chain, used_words, trie_root)
-
-            current_chain.pop()
-            used_words.remove(next_word)
-
-
-def find_longest_chain(start_word, all_words):
-    global longest_chain_found
-
-    trie_root = TrieNode()
-    for word in all_words:
-        insert_word(trie_root, word)
-
-    if start_word not in all_words:
-        return [start_word]
-
-    used_words = {start_word}
-    current_chain = [start_word]
-    longest_chain_found = [start_word]
-
-    backtrack(current_chain, used_words, trie_root)
-
-    return longest_chain_found
+        self._backtrack([start_word], used_words)
+        return self.longest_chain
 
 
 if __name__ == "__main__":
 
-    try:
-        N = int(input())
-        word_list = [input() for _ in range(N)]
-        start = input()
+    N = int(input())
+    word_list = {input() for _ in range(N)}
+    start = input()
 
-        result_chain = find_longest_chain(start, set(word_list))
-        print(" ".join(result_chain))
+    solver = WordChainSolver()
+    result_chain = solver.find_longest_chain(start, word_list)
 
-    except (IOError, ValueError):
-
-        word_list = ["apple", "ear", "rat", "tiger", "rhino", "orange"]
-        start = "apple"
-
-        result_chain = find_longest_chain(start, set(word_list))
-        print(" ".join(result_chain))
+    print(" ".join(result_chain))
