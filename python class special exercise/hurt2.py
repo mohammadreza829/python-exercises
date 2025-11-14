7cb5af3374498be428228cc98ee7a32a522ec22e
mohammadreza <mohammadrezafasli1234@gmail.com>
2025-11-14 17:14:32 +0330

    hurt by for

---
+++ python class special exercise/hurt2.py
@@ -0,0 +1,13 @@
+
+for y in range(15, -15, -1):
+    line = ""
+    for x in range(-30, 30):
+
+        scaled_x = x * 0.04
+        scaled_y = y * 0.1
+
+        if ((scaled_x**2 + scaled_y**2 - 1) ** 3 - scaled_x**2 * scaled_y**3) <= 0:
+            line += "♥"
+        else:
+            line += " "
+    print(line)
