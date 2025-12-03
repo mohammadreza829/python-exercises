cc252dce4a50e481348b3e29ac7761664929dd50
mohammadreza <mohammadrezafasli1234@gmail.com>
2025-12-03 15:09:13 +0330

    bc

--- data structure exercises/data structure/week8/w8-q7-mohammadreza-fasli.py
+++ data structure exercises/data structure/week8/w8-q7-mohammadreza-fasli.py
@@ -5,16 +5,12 @@ def count_accepted(desc_scores, limit):
     while left <= right:
         mid = (left + right) // 2
         if desc_scores[mid] >= limit:
-            # این دانشجو شرایط را دارد، سعی می‌کنیم ببینیم آیا سمت راست هم هنوز شرایط دارند یا نه
             last_ok = mid
             left = mid + 1
         else:
-            # این نمره زیر حد است، باید سمت چپ را چک کنیم
             right = mid - 1
 
-    # اگر هیچ‌کسی شرایط نداشت، last_ok = -1 → تعداد = 0
     return last_ok + 1
-
 
 if __name__ == "__main__":
     line = input().strip()
