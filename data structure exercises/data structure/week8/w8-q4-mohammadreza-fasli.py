9e36ca5da676995d3c3f8199b5270e8df27a73f5
mohammadreza <mohammadrezafasli1234@gmail.com>
2025-12-04 16:12:11 +0330

    new way q4

--- data structure exercises/data structure/week8/w8-q4-mohammadreza-fasli.py
+++ data structure exercises/data structure/week8/w8-q4-mohammadreza-fasli.py
@@ -2,21 +2,22 @@ def quick_sort_projects(items):
     if len(items) <= 1:
         return items
 
-    pivot = items[len(items) // 2]  
+    pivot = items[len(items) // 2]
     less = []
     equal = []
     greater = []
 
     for x in items:
-        if x[0] < pivot[0]:
+        if x[1] < pivot[1]:      
             less.append(x)
-        elif x[0] > pivot[0]:
+        elif x[1] > pivot[1]:
             greater.append(x)
         else:
             equal.append(x)
 
     return quick_sort_projects(less) + equal + quick_sort_projects(greater)
 
+ 
 if __name__ == "__main__":
     n = int(input().strip())
     projects = []
@@ -24,11 +25,12 @@ if __name__ == "__main__":
     for _ in range(n):
         line = input().rstrip()
         parts = line.split()
-        complexity = int(parts[0])
+        score = int(parts[0])
         name = " ".join(parts[1:])
-        projects.append((complexity, name))
+        projects.append((name, score))
 
+    
     sorted_projects = quick_sort_projects(projects)
 
-    for complexity, name in sorted_projects:
-        print(f"{complexity} {name}")
+    for name, score in sorted_projects:
+        print(f"{name} , {score}")
