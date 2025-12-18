149e4235e648f6b45836c840e5c817140b966129
mohammadreza <mohammadrezafasli1234@gmail.com>
2025-12-18 12:40:57 +0330

    class bank account

---
+++ python class special exercise/BankAccount.py
@@ -0,0 +1,26 @@
+import random
+class BankAccount:
+    def __init__(self , owner_name , initial_balance , __account_number , __balance):
+        self.owner_name = owner_name
+        self.initial_balance = initial_balance
+        self.__account_number = __account_number
+        self.__balance = __balance
+    
+    def __generate_account_number(self):
+        return random.randint(1000000000 , 9999999999)
+    
+    def deposit(self , amount):
+        self.__balance += amount
+    
+    def withdraw(self , amount):
+        self.__balance -= amount
+    
+    def get_balance(self):
+        return self.__balance
+    
+    def display_info(self):
+        print(f"Owner Name: {self.owner_name}")
+        print(f"Account Number: {self.__account_number}")
+        print(f"Balance: {self.__balance}")
+    
+    
